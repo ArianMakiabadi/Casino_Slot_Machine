86072a50f4f40c2d0cf3771b5084344e7443aec8
arian <arian.maki@yahoo.com>
2025-05-26 16:27:28 +0200

    MultiBandit and BanditApp classes added

---
+++ src/ai_bandit/lab1/BanditApp.java
@@ -0,0 +1,5 @@
+package ai_bandit.lab1;
+
+public class BanditApp {
+    
+}
---
+++ src/ai_bandit/lab1/MultiBandit.java
@@ -0,0 +1,21 @@
+package ai_bandit.lab1;
+
+public class MultiBandit {
+    // Attributes
+    private Bandit[] bandits;
+
+    // Constructor
+    public MultiBandit(int numberBandits) {
+        for (int i = 0; i< numberBandits; i++){
+
+        }
+    }
+
+    // Getters
+    public int getNumberBandits(){
+    }
+
+    public double getPricePerRound(){
+
+    }
+}
