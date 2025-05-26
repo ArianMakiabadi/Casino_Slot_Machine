ad2a5b53e702c4362a3dfcdc8cdc1265fa323fc4
arian <arian.maki@yahoo.com>
2025-05-26 12:31:01 +0200

    attributes added

--- src/Game/Bandit.java
+++ src/Game/Bandit.java
@@ -1,4 +1,14 @@
 package Game;
 
+import java.util.Random;
+
 public class Bandit {
+    //private attributes
+    private Random random;
+    private String name;
+    private double pricePerRound;
+    private double averageWin;
+    private double stdDevWin;
+    private double overallProfit;
+    private int roundsPlayed;
 }
