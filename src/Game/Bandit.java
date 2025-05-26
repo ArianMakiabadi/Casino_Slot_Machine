32e975b94dc79b2ebc9f7a806481423af9685619
arian <arian.maki@yahoo.com>
2025-05-26 12:35:36 +0200

    constructors added

--- src/Game/Bandit.java
+++ src/Game/Bandit.java
@@ -11,4 +11,20 @@ public class Bandit {
     private double stdDevWin;
     private double overallProfit;
     private int roundsPlayed;
+
+    // Constructor with name
+    public Bandit(String name, double pricePerRound, double averageWin, double stdDevWin) {
+        this.name = name;
+        this.pricePerRound = pricePerRound;
+        this.averageWin = averageWin;
+        this.stdDevWin = stdDevWin;
+        this.random = new Random();
+        this.overallProfit = 0.0;
+        this.roundsPlayed = 0;
+    }
+
+    // Constructor without name
+    public Bandit(double pricePerRound, double averageWin, double stdDevWin) {
+        this("One-Armed Bandit", pricePerRound, averageWin, stdDevWin);
+    }
 }
