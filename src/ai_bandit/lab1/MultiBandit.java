43f6f23fabbb8187877f78c364723beafc2bad09
arian <arian.maki@yahoo.com>
2025-06-04 10:24:29 +0200

    constructor added

--- src/ai_bandit/lab1/MultiBandit.java
+++ src/ai_bandit/lab1/MultiBandit.java
@@ -2,17 +2,31 @@ package ai_bandit.lab1;
 
 public class MultiBandit {
     // Attributes
+    private int numberBandits;
     private Bandit[] bandits;
 
     // Constructor
     public MultiBandit(int numberBandits) {
-        for (int i = 0; i< numberBandits; i++){
+        this.numberBandits = numberBandits;
+        this.bandits = new Bandit[numberBandits];
+        int winner = (int)(Math.random() * (double)numberBandits);
 
+        for(int i = 0; i < numberBandits; ++i) {
+            double averageWin;
+            if (winner == i) {
+                averageWin = Math.random() * 0.2D + 1.1D;
+            } else {
+                averageWin = Math.random() * 0.3D + 0.5D;
+            }
+
+            this.bandits[i] = new Bandit(1.0D, averageWin, 1.0D);
         }
+
     }
 
     // Getters
     public int getNumberBandits(){
+
     }
 
     public double getPricePerRound(){
