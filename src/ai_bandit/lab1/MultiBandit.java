6b087b20d93d36c64dab6a72c8e4e82f956b3354
arian <arian.maki@yahoo.com>
2025-06-16 01:04:55 +0200

    Adopted to Bandit class

--- src/ai_bandit/lab1/MultiBandit.java
+++ src/ai_bandit/lab1/MultiBandit.java
@@ -2,47 +2,42 @@ package ai_bandit.lab1;
 
 public class MultiBandit {
     // Attributes
-    private int numberBandits;
     private Bandit[] bandits;
 
     // Constructor
     public MultiBandit(int numberBandits) {
-        this.numberBandits = numberBandits;
-        this.bandits = new Bandit[numberBandits];
+        bandits = new Bandit[numberBandits];
         int winner = (int)(Math.random() * (double)numberBandits);
 
-        for(int i = 0; i < numberBandits; ++i) {
-            double averageWin;
-            if (winner == i) {
-                averageWin = Math.random() * 0.2D + 1.1D;
-            } else {
-                averageWin = Math.random() * 0.3D + 0.5D;
-            }
+        for (int i = 0; i < bandits.length; ++i) {
+            double averageWin = (i == winner)
+                    ? Math.random() * 0.2 + 1.1
+                    : Math.random() * 0.3 + 0.5;
 
-            this.bandits[i] = new Bandit(1.0D, averageWin, 1.0D);
+            bandits[i] = new Bandit(1.0, averageWin, 1.0);
         }
 
     }
 
     // Getters
     public int getNumberBandits() {
-        return this.numberBandits;
+        return bandits.length;
     }
 
     public double getPricePerRound() {
         double totalPrice = 0.0D;
 
-        for(int i = 0; i < this.numberBandits; ++i) {
+        for(int i = 0; i < this.getNumberBandits(); i++) {
             totalPrice += this.bandits[i].getPricePerRound();
         }
 
-        return totalPrice / (double)this.numberBandits;
+        return totalPrice / (double)this.getNumberBandits();
     }
 
     public double getOverallProfit() {
         double totalProfit = 0.0D;
 
-        for(int i = 0; i < this.numberBandits; ++i) {
+        for(int i = 0; i < this.getNumberBandits(); ++i) {
             totalProfit += this.bandits[i].getOverallProfit();
         }
 
@@ -56,7 +51,7 @@ public class MultiBandit {
     public int getRoundsPlayed() {
         int totalRoundsPlayed = 0;
 
-        for(int i = 0; i < this.numberBandits; ++i) {
+        for(int i = 0; i < this.getNumberBandits(); ++i) {
             totalRoundsPlayed += this.bandits[i].getRoundsPlayed();
         }
 
